b3abc28a1dd7e17aa502d86f1ffff96a10283f14
rahulhingve <rahulpawar2001.rp@gmail.com>
2024-06-02 14:43:11 +0530

    All fake routed handled and routed  to /blog

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { BrowserRouter, Navigate, Route,  Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { Signin } from './pages/Signin'
 import { Signup } from './pages/Signup'
 import { Blog } from './pages/Blog'
@@ -8,27 +8,28 @@ import { Blogs } from './pages/Blogs'
 import { Publish } from './pages/Publish'
 
 function App() {
-  
+
 
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/signup" element={<Signup/>}></Route>
-      <Route path="/signin" element={<Signin/>}></Route>
-      <Route path="/blog/:id" element={<Blog/>}></Route>
-      <Route path="/blogs" element={<Blogs/>}></Route>
-      <Route path="/publish" element={<Publish/>}></Route>
-      {/* Default route to /signin */}
-      <Route path="/" element={<Navigate to="/signin" replace />} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/blog/:id" element={<Blog />}></Route>
+          <Route path="/blogs" element={<Blogs />}></Route>
+          <Route path="/publish" element={<Publish />}></Route>
+          {/* Default route to /signin */}
+          <Route path="/" element={<Navigate to="/signin" replace />} />
+
+          {/* Catch all route to redirect to /blogs */}
+          <Route path="*" element={<Navigate to="/blogs" replace />} />
+        </Routes>
 
 
-    </Routes>
-    
-    
-    </BrowserRouter>
-    
-    
+      </BrowserRouter>
+
+
     </>
   )
 }
